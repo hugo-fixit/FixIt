1a8d4907ef0a77447a2e01d76f00546eb06c3628
Cell <1024@lruihao.cn>
2022-01-09 22:42:29 +0800

    :sparkles: Feat: add CustomJS option (#24)

---
+++ assets/js/_custom.js
@@ -0,0 +1,36 @@
+/**
+ * @author @Lruihao https://lruihao.cn
+ * @description Custom javascript for the hugo theme FixIt.
+ */
+const CustomJS = new (function () {
+  /**
+   * Hello World
+   * You can define your own functions below.
+   * @returns {CustomJS}
+   */
+  this.hello = () => {
+    console.log('hello CustomJS!');
+    return this;
+  }
+  /**
+   * Initialize.
+   * @returns {CustomJS}
+   */
+  this.init = () => {
+    // Custom infos.
+    this.hello();
+    return this;
+  };
+})();
+
+/**
+ * Immediate.
+ */
+(() => {
+  CustomJS.init();
+  // It will be executed when the DOM tree is built.
+  document.addEventListener('DOMContentLoaded', () => {
+    // Listen to any DOM change and automatically perform spacing via MutationObserver()
+    console.log('DOMContentLoaded!')
+  });
+})();
