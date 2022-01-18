3698ebf281ca00774c7fcd156f6041eff03fe8bb
Cell <1024@lruihao.cn>
2022-01-18 21:40:28 +0800

    :pencil2: Style: fix typos

--- fixit_checker.sh
+++ fixit_checker.sh
@@ -1,5 +1,5 @@
 echo "Checking for FixIt updates ..."
 cd ../..
-git submodule update --remote 
+git submodule update --remote
 git add themes/FixIt
 git commit -m ":arrow_up: Chore: update FixIt theme version"
