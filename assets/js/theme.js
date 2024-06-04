cbde3f08985d947e587f5768fc481e5b320d19ec
Cell <1024@lruihao.cn>
2024-06-04 15:23:29 +0800

    :children_crossing: Feat: replace localStorage with sessionStorage for FixIt auto bookmark

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1039,9 +1039,9 @@ class FixIt {
       return;
     }
     window.addEventListener('beforeunload', () => {
-      window.localStorage?.setItem(`fixit-bookmark/#${location.pathname}`, this.util.getScrollTop());
+      window.sessionStorage?.setItem(`fixit-bookmark/#${location.pathname}`, this.util.getScrollTop());
     });
-    const scrollTop = Number(window.localStorage?.getItem(`fixit-bookmark/#${location.pathname}`));
+    const scrollTop = Number(window.sessionStorage?.getItem(`fixit-bookmark/#${location.pathname}`));
     // If the page opens with a specific hash, just jump out
     if (scrollTop && location.hash === '') {
       window.scrollTo({ 
