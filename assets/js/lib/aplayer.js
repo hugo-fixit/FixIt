a6debff87c11e92d92f91fda79f8b1e20f33653f
Cell <1024@lruihao.cn>
2025-10-21 15:45:43 +0800

    feat: initialize APlayer when content decrypted

--- assets/js/lib/aplayer.js
+++ assets/js/lib/aplayer.js
@@ -1,10 +1,19 @@
-const aplayerInstances = []
+window.FixItAPlayer = {
+  init: () => {
+    Array.from(document.getElementsByClassName("aplayer-shortcode")).forEach((aplayer) => {
+      if (aplayer.dataset.processed) return
+      const audio = JSON.parse(aplayer.dataset.audio)
+      const options = JSON.parse(aplayer.dataset.options)
+      options.audio = audio
+      options.container = aplayer
+      new APlayer(options)
+      aplayer.dataset.processed = true
+    })
+  },
+}
 
-Array.from(document.getElementsByClassName("aplayer-shortcode")).forEach((aplayer) => {
-  const audio = JSON.parse(aplayer.dataset.audio)
-  const options = JSON.parse(aplayer.dataset.options)
-  options.audio = audio
-  options.container = aplayer
-  const ap = new APlayer(options)
-  aplayerInstances.push(ap)
-})
+if (document.readyState !== 'loading') {
+  window.FixItAPlayer.init()
+} else {
+  document.addEventListener('DOMContentLoaded', window.FixItAPlayer.init, false)
+}
